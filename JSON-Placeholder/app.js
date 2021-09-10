b3e603d4d25ed86127d1a4a749196d30a6e357d0
naimsiddiquibd <naimsiddiquibd@gmail.com>
2021-09-11 00:16:18 +0600

    json-placeholder

--- JSON-Placeholder/app.js
+++ JSON-Placeholder/app.js
@@ -7,11 +7,20 @@ function loadData(){
 function loadUsers(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayUsers(data))
 }
 
 function loadPost(){
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
     .then(data => console.log(data))
+}
+
+function displayUsers(data){
+    const ul = document.getElementById('users');
+    for (const user of data){
+        const li = document.createElement('li');
+        li.innerText = `Name: ${user.name}`;
+        ul.appendChild(li);
+    }
 }
